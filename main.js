4fc9e2af5a7d1310db45143fab3f889ef70df72b
PlzDonateRobuxCuzImPoor <153300591+KeaseyHu994@users.noreply.github.com>
2024-05-28 10:04:02 -0400

    complete

--- main.js
+++ main.js
@@ -3,38 +3,29 @@
 //Transator Project
 
 function checkLang(){
-    let text;
-    let language = document.querySelector("#lang").value;
+    let language = document.querySelector("#lang").value.toLowerCase();
 
     switch(language){
         case "french":
-            text = "'Bonjour!' means hello in French.";
+            document.querySelector("#translation").innerHTML = "'Bonjour!' means hello in French.";
             break;
         case "german":
-            text = "'Guten Tag!' means hello in German.";
+            document.querySelector("#translation").innerHTML = "'Guten Tag!' means hello in German.";
             break;
         case "dutch":
-            text = "'Goededag!'means hello in Dutch.";
+            document.querySelector("#translation").innerHTML = "'Goededag!'means hello in Dutch.";
             break;
         default:
-            text = "Sorry, but we don't teach that language at your school!";
+            document.querySelector("#translation").innerHTML = "Sorry, but we don't teach that language at your school!";
     }
-
-    document.querySelector("#translation").innerHTML = text;
-}
-
-function resetParagraph(){
-    document.querySelector("#translation").innerHTML = "";
-}
-
-function resetInputField(){
-    document.querySelector("#translation").innerHTML = "";
 }
 
 document.querySelector('#resetAll').addEventListener('click', () => {
-    resetParagraph();
-    resetInputField();
+    document.querySelector("#translation").innerHTML = "";
+    document.querySelector("#lang").value = "";
 });
+
+document.querySelector('#check').addEventListener('click', checkLang);
 
 let instructions = "Which language do you want to learn?";
 let student = "by Hunter Keasey";
